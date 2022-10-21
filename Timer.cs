5c98019dfe8e413ff28de1096b5f0a16b09b99cc
BSH062 <116258825+BSH062@users.noreply.github.com>
2022-10-22 03:48:42 +0900

    UI 관련

---
+++ Timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+    public Text _timer;
+    private float _timerForText;
+    private int _secText;
+    private int _minText;
+
+	void Update () {
+
+        _timerForText += Time.deltaTime;
+
+        if (_timerForText > 1.0f)
+        {
+            _secText += 1;
+            if (_secText > 60)
+            {
+                _minText += 1;
+                _secText = 0;
+            }
+            _timer.text = string.Format("{0:D2}", _minText) + ":" + string.Format("{0:D2}", _secText);
+            
+            _timerForText = 0;
+        }
+
+	
+	}
+}
