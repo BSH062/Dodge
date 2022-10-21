71eea43b11ecd2a08244cade9430ee2ab97bc6f3
tjrgus <tjrgus062@gmail.com>
2022-10-22 00:13:43 +0900

    테스트 커밋

---
+++ Assets/3_Script/EnemyAttackScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttackScript : MonoBehaviour {
+	
+	public EnemyScript _EnemySt;
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "player")
+        {
+            
+			other.SendMessage("Damaged",1.0f);
+        }
+        
+    }
+}
